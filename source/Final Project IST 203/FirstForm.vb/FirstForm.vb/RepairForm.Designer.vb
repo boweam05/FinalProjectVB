46cb65678d23e8c62df478b86a170446e712d54d
DESKTOP-IUTED3D\ausbo <boweam05@students.ipfw.edu>
2018-04-01 12:03:48 -0400

    Added a repair form. This form consisted of all the types of repairs I will be using. I have not figured out what prices to choose yet.

---
+++ source/Final Project IST 203/FirstForm.vb/FirstForm.vb/RepairForm.Designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class RepairForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.chkScreenRepair = New System.Windows.Forms.CheckBox()
+        Me.chkBatteryReplacment = New System.Windows.Forms.CheckBox()
+        Me.chkChargingPortRepair = New System.Windows.Forms.CheckBox()
+        Me.chkHeadphonejackRepair = New System.Windows.Forms.CheckBox()
+        Me.chkCameraRepair = New System.Windows.Forms.CheckBox()
+        Me.chkOutterShellReplace = New System.Windows.Forms.CheckBox()
+        Me.chkPasswordRemoval = New System.Windows.Forms.CheckBox()
+        Me.chkVirusRemoval = New System.Windows.Forms.CheckBox()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(49, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(165, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "We Have These Repairs For You"
+        '
+        'chkScreenRepair
+        '
+        Me.chkScreenRepair.AutoSize = True
+        Me.chkScreenRepair.Location = New System.Drawing.Point(34, 83)
+        Me.chkScreenRepair.Name = "chkScreenRepair"
+        Me.chkScreenRepair.Size = New System.Drawing.Size(94, 17)
+        Me.chkScreenRepair.TabIndex = 1
+        Me.chkScreenRepair.Text = "Screen Repair"
+        Me.chkScreenRepair.UseVisualStyleBackColor = True
+        '
+        'chkBatteryReplacment
+        '
+        Me.chkBatteryReplacment.AutoSize = True
+        Me.chkBatteryReplacment.Location = New System.Drawing.Point(34, 117)
+        Me.chkBatteryReplacment.Name = "chkBatteryReplacment"
+        Me.chkBatteryReplacment.Size = New System.Drawing.Size(125, 17)
+        Me.chkBatteryReplacment.TabIndex = 2
+        Me.chkBatteryReplacment.Text = "Battery Replacement"
+        Me.chkBatteryReplacment.UseVisualStyleBackColor = True
+        '
+        'chkChargingPortRepair
+        '
+        Me.chkChargingPortRepair.AutoSize = True
+        Me.chkChargingPortRepair.Location = New System.Drawing.Point(34, 152)
+        Me.chkChargingPortRepair.Name = "chkChargingPortRepair"
+        Me.chkChargingPortRepair.Size = New System.Drawing.Size(124, 17)
+        Me.chkChargingPortRepair.TabIndex = 3
+        Me.chkChargingPortRepair.Text = "Charging Port Repair"
+        Me.chkChargingPortRepair.UseVisualStyleBackColor = True
+        '
+        'chkHeadphonejackRepair
+        '
+        Me.chkHeadphonejackRepair.AutoSize = True
+        Me.chkHeadphonejackRepair.Location = New System.Drawing.Point(34, 186)
+        Me.chkHeadphonejackRepair.Name = "chkHeadphonejackRepair"
+        Me.chkHeadphonejackRepair.Size = New System.Drawing.Size(142, 17)
+        Me.chkHeadphonejackRepair.TabIndex = 4
+        Me.chkHeadphonejackRepair.Text = "Headphone Jack Repair"
+        Me.chkHeadphonejackRepair.UseVisualStyleBackColor = True
+        '
+        'chkCameraRepair
+        '
+        Me.chkCameraRepair.AutoSize = True
+        Me.chkCameraRepair.Location = New System.Drawing.Point(34, 221)
+        Me.chkCameraRepair.Name = "chkCameraRepair"
+        Me.chkCameraRepair.Size = New System.Drawing.Size(96, 17)
+        Me.chkCameraRepair.TabIndex = 5
+        Me.chkCameraRepair.Text = "Camera Repair"
+        Me.chkCameraRepair.UseVisualStyleBackColor = True
+        '
+        'chkOutterShellReplace
+        '
+        Me.chkOutterShellReplace.AutoSize = True
+        Me.chkOutterShellReplace.Location = New System.Drawing.Point(34, 256)
+        Me.chkOutterShellReplace.Name = "chkOutterShellReplace"
+        Me.chkOutterShellReplace.Size = New System.Drawing.Size(147, 17)
+        Me.chkOutterShellReplace.TabIndex = 6
+        Me.chkOutterShellReplace.Text = "Outter Shell Replacement"
+        Me.chkOutterShellReplace.UseVisualStyleBackColor = True
+        '
+        'chkPasswordRemoval
+        '
+        Me.chkPasswordRemoval.AutoSize = True
+        Me.chkPasswordRemoval.Location = New System.Drawing.Point(34, 290)
+        Me.chkPasswordRemoval.Name = "chkPasswordRemoval"
+        Me.chkPasswordRemoval.Size = New System.Drawing.Size(117, 17)
+        Me.chkPasswordRemoval.TabIndex = 7
+        Me.chkPasswordRemoval.Text = "Password Removal"
+        Me.chkPasswordRemoval.UseVisualStyleBackColor = True
+        '
+        'chkVirusRemoval
+        '
+        Me.chkVirusRemoval.AutoSize = True
+        Me.chkVirusRemoval.Location = New System.Drawing.Point(34, 323)
+        Me.chkVirusRemoval.Name = "chkVirusRemoval"
+        Me.chkVirusRemoval.Size = New System.Drawing.Size(94, 17)
+        Me.chkVirusRemoval.TabIndex = 8
+        Me.chkVirusRemoval.Text = "Virus Removal"
+        Me.chkVirusRemoval.UseVisualStyleBackColor = True
+        '
+        'btnNext
+        '
+        Me.btnNext.Location = New System.Drawing.Point(163, 335)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(75, 23)
+        Me.btnNext.TabIndex = 9
+        Me.btnNext.Text = "Next"
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'RepairForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(250, 370)
+        Me.Controls.Add(Me.btnNext)
+        Me.Controls.Add(Me.chkVirusRemoval)
+        Me.Controls.Add(Me.chkPasswordRemoval)
+        Me.Controls.Add(Me.chkOutterShellReplace)
+        Me.Controls.Add(Me.chkCameraRepair)
+        Me.Controls.Add(Me.chkHeadphonejackRepair)
+        Me.Controls.Add(Me.chkChargingPortRepair)
+        Me.Controls.Add(Me.chkBatteryReplacment)
+        Me.Controls.Add(Me.chkScreenRepair)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "RepairForm"
+        Me.Text = "RepairForm"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents chkScreenRepair As CheckBox
+    Friend WithEvents chkBatteryReplacment As CheckBox
+    Friend WithEvents chkChargingPortRepair As CheckBox
+    Friend WithEvents chkHeadphonejackRepair As CheckBox
+    Friend WithEvents chkCameraRepair As CheckBox
+    Friend WithEvents chkOutterShellReplace As CheckBox
+    Friend WithEvents chkPasswordRemoval As CheckBox
+    Friend WithEvents chkVirusRemoval As CheckBox
+    Friend WithEvents btnNext As Button
+End Class
---
+++ source/Final Project IST 203/FirstForm.vb/FirstForm.vb/RepairForm.vb
@@ -0,0 +1,3 @@
+﻿Public Class RepairForm
+
+End Class
